74114c178db69c69b37106d2a296f7d8ff3e3325
Muhammad Ali <ali@esparkconsultants.com>
2023-03-30 01:15:05 +0500

    Chapter 2 task done

---
+++ Chapter 2/index.js
@@ -0,0 +1,36 @@
+// Question 1
+var username;
+
+// Question 2 
+var myName="Kashaf Zehra";
+
+// Question 3
+var message="Hello World";
+alert(message);
+
+// Question 4
+var studentName = "Ali";
+var studentAge = 25;
+var designation = "Certified Mobile App DEvelopment";
+alert(studentName);
+alert(studentAge);
+alert(designation);
+
+// Question 5
+var FavFood=" PIZZA \n PIZZ  \n PIZ  \n PI \n P";
+alert(FavFood)
+
+// Question 6
+var email="My email address is "+"kashaf@gmail.com";
+alert(email);
+
+// Question 7
+var message="A smarter way to learn JavaScript";
+alert(message)
+
+// Question 8 
+document.write("Yah! I can write HTML content through javaScript");
+
+// Question 9 
+var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design); 
