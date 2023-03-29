aeb9036ffc8ee5397e6999516f99620eaf34d940
Muhammad Ali <ali@esparkconsultants.com>
2023-03-30 00:54:37 +0500

    Chapter 1 task done

---
+++ Chapter 1/index.js
@@ -0,0 +1,11 @@
+// Question 1
+alert("Welcome to my learning Js");
+// Question 2
+alert("Error! Please enter a valid password");
+// Question 3
+alert("Welcome to JS Land \n Happy Coding" );
+// Question 4 
+alert("Welcome to JS Land....");
+// Question 5
+var message = "Hello...I can run JS through my web browser's console" ; 
+alert(message,console.log(message));
