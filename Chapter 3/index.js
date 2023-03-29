b5a577e8dcdbcdb92778821b640da93c03d4ba6c
Muhammad Ali <ali@esparkconsultants.com>
2023-03-30 01:28:46 +0500

    Chapter 3 task done

---
+++ Chapter 3/index.js
@@ -0,0 +1,18 @@
+// Question 1
+var age = 24;
+alert("I'm " + age + " year old");
+
+// Question 2 
+var visitorTime = 14;
+alert("You have visited this site " + visitorTime + " times");
+
+// Question 3
+var birthYear = 1997;
+document.write("My birth year is " + birthYear + "<br>");
+ 
+// Question 4
+var visitorName="Hina";
+var Product=" Jeans ";
+var Qunatity = 5;
+document.write(visitorName + " ordered " + Qunatity + Product + " on XYZ Clothing store " + "<br>");
+
