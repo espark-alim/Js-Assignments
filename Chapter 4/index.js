91df71fbe1122e51405fae2fedee3055671edf24
Muhammad Ali <ali@esparkconsultants.com>
2023-03-30 21:50:32 +0500

    Chapter 4 task done

---
+++ Chapter 4/index.js
@@ -0,0 +1,15 @@
+// Question 1
+var name,age,designation;
+
+// Question 2
+var _num = "numbers";
+var $dollar = "$";
+var _letter = "letter";
+var $sensitive = "sensitive";
+var _keywords = "keywords";
+
+// Question 3
+document.write("Variable names can only contain"+ _num +" "+ $dollar + "<br>");
+document.write("Variable must be begin with a " + _letter + " " + $dollar + "<br>");
+document.write("Variable names are " + $sensitive + "<br>");
+document.write("Variable names should not be "+ _keywords + "<br>");
